71e9c5d4131bc5d277bf26c92781df8f11992aed
Naremancil <hnefer@hotmail.com>
2024-11-13 20:28:37 -0300

    Salvando a escolha de modo escuro ou não

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-document.querySelector('#dark-mode').addEventListener('change', () =>{
+if(localStorage.getItem('isDarkMode') == 1){
     document.querySelector("body").classList.toggle("dark-mode")
+}
+
+document.querySelector('#dark-mode').addEventListener('change', () => {
+    document.querySelector("body").classList.toggle("dark-mode")
+    if(localStorage.getItem('isDarkMode') == 1){
+        localStorage.setItem('isDarkMode', 0)
+    } else localStorage.setItem('isDarkMode', 1)
 })
 
 
