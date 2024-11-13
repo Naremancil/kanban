6efb0b831cdbcc42727b8c5b0d7fc95bb9e4bb8f
Naremancil <hnefer@hotmail.com>
2024-11-13 20:21:18 -0300

    Criação do modo noturno

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector('#dark-mode').addEventListener('change', () =>{
+    document.querySelector("body").classList.toggle("dark-mode")
+})
+
+
+async function checkEmail(email){
+
+}
